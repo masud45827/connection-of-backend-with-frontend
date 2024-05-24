6888db863445742dd85ae2371bb78960c61aa391
masud45827 <masud.cse2019@gmail.com>
2024-05-24 23:49:54 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const Koa =require('koa');
+const dotenv = require('dotenv');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser')
+const cors = require('@koa/cors');
+const usersRoutes = require('./routes/userRoutes');
+
+
+dotenv.config();
+const app = new Koa();
+const port  = process.env.PORT;
+const router = new Router();
+app.use(bodyParser());
+app.use(cors());
+
+router.use('/users',usersRoutes);
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+
+app.listen(3000,()=>{
+    console.log(`running port ${port}`);
+})
